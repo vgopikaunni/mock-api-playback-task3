f52e1b9ae9fde10cc716a0c2a2bf3ecf0467a9a4
Alex <mathewalex806@gmail.com>
2025-06-06 12:10:11 +0530

    Profiles

--- src/main/java/com/alex/example/ExampleApplication.java
+++ src/main/java/com/alex/example/ExampleApplication.java
@@ -1,5 +1,6 @@
 package com.alex.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,14 +8,17 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class ExampleApplication {
 
+
 	public static void main(String[] args)
 	{
 
 		var ctx = SpringApplication.run(ExampleApplication.class, args);
-
 		MyFirstService obj1 = ctx.getBean(MyFirstService.class);
 		 System.out.println(obj1.tellStory());
 		 System.out.println(obj1.JavaVersion());
+		 System.out.println(obj1.readProperty());
+		 System.out.println(obj1.getMyName());
+		 System.out.println(obj1.getLocation());
 	}
 
 
--- src/main/java/com/alex/example/MyFirstService.java
+++ src/main/java/com/alex/example/MyFirstService.java
@@ -3,14 +3,22 @@ package com.alex.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
+@PropertySource("classpath:custom.properties")
 public class MyFirstService
 {
+    @Value("${my.location}")
+    private String customProperty ;
     private MyFirstClass myFirstClass ;
     private Environment env;
+    @Value("${my.name}")
+    private String myName;
 
     @Autowired
     public void setMyFirstclass(@Qualifier("MyFirstBean") MyFirstClass myFirstClass)
@@ -33,4 +41,21 @@ public class MyFirstService
     {
         this.env = env;
     }
+
+    public String readProperty()
+    {
+        return env.getProperty("my.custom.property");
+    }
+
+    public String getMyName()
+    {
+        return myName;
+    }
+
+    public String getLocation()
+    {
+        return customProperty;
+    }
+
+
 }
